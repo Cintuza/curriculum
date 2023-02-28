fe84f4450cb2529063496132c027d92cd3885668
cintuza <cinbiblio@yahoo.com.ar>
2023-02-28 20:54:05 -0300

    experiencia profesional

--- src/main.js
+++ src/main.js
@@ -8,6 +8,7 @@ var fechaDeNacimiento = new Date(1970,5,5);   // 5/6/1970
 var direccion = "9515 W Gray St";
 var telefono = "(622) 655-7281";
 
+
 document.getElementById('nombre').innerHTML = nombre + " " + apellido;
 document.getElementById('email').innerHTML = email;
 document.getElementById('fechaDeNacimiento').innerHTML = fechaDeNacimiento.getDay()
@@ -25,5 +26,6 @@ function counter() {
     document.getElementById('app').innerHTML = `<p>You have been here for ${seconds} seconds.</p>`;
   }, 1000);
 }
+
 
 counter();
