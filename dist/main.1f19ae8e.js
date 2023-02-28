27b6cd5425a511aad1ffabeb0b2f91e2510b7fe9
cintuza <cinbiblio@yahoo.com.ar>
2023-02-27 21:28:51 -0300

    datos personales y foto

--- dist/main.1f19ae8e.js
+++ dist/main.1f19ae8e.js
@@ -120,6 +120,20 @@ parcelRequire = (function (modules, cache, entry, globalName) {
 })({"main.js":[function(require,module,exports) {
 'use strict';
 
+// Datos personales tomados de: https://randomuser.me/
+var nombre = "Violet";
+var apellido = "García";
+var email = "violet.garcia@example.com";
+var fechaDeNacimiento = new Date(1970, 5, 5); // 5/6/1970
+var direccion = "9515 W Gray St";
+var telefono = "(622) 655-7281";
+document.getElementById('nombre').innerHTML = nombre + " " + apellido;
+document.getElementById('email').innerHTML = email;
+document.getElementById('fechaDeNacimiento').innerHTML = fechaDeNacimiento.getDay() + " de " + fechaDeNacimiento.toLocaleString('default', {
+  month: 'long'
+}) + " de " + fechaDeNacimiento.getFullYear();
+document.getElementById('direccion').innerHTML = direccion;
+document.getElementById('telefono').innerHTML = telefono;
 function counter() {
   var seconds = 0;
   setInterval(function () {
@@ -153,7 +167,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "38223" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "35289" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
--- src/main.js
+++ src/main.js
@@ -1,5 +1,23 @@
 'use strict';
 
+// Datos personales tomados de: https://randomuser.me/
+var nombre = "Violet";
+var apellido = "García";
+var email = "violet.garcia@example.com";
+var fechaDeNacimiento = new Date(1970,5,5);   // 5/6/1970
+var direccion = "9515 W Gray St";
+var telefono = "(622) 655-7281";
+
+document.getElementById('nombre').innerHTML = nombre + " " + apellido;
+document.getElementById('email').innerHTML = email;
+document.getElementById('fechaDeNacimiento').innerHTML = fechaDeNacimiento.getDay()
+          + " de " + fechaDeNacimiento.toLocaleString('default', { month: 'long' })
+          + " de " + fechaDeNacimiento.getFullYear();
+document.getElementById('direccion').innerHTML = direccion;
+document.getElementById('telefono').innerHTML = telefono;
+
+
+
 function counter() {
   let seconds = 0;
   setInterval(() => {
